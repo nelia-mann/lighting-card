ec59d68cfaf3100bd19b8b2c49eb2e85e69bd2a2
Nelia Mann <mannn@union.edu>
2026-01-25 23:52:42 +0000

    attached select item to light item

--- src/main.js
+++ src/main.js
@@ -68,7 +68,6 @@ export class MainCard extends LitElement {
                 selects.forEach((select) => {
                     if (select.entity_id.includes(light_id)) {
                         value.select = select
-                        console.log(value);
                     }
                 })
                 lightEntities.push(value)
