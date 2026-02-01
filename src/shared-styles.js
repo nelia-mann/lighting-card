78fede9112d6d128596e6a801eaed949c7b6b4c1
Nelia Mann <mannn@union.edu>
2026-02-01 17:35:12 +0000

    created new shared styles sheet

---
+++ src/shared-styles.js
@@ -0,0 +1,25 @@
+import { css } from 'lit';
+
+export default css`
+
+    .outlined {
+        border: 1px solid;
+        border-radius: 12px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .small-heading {
+        font-weight: 700;
+        font-size: 100%;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .sub-info {
+        padding: 0px;
+        margin: 0px;
+        font-weight: 400;
+        font-size: 100%;
+    }
+
+`
